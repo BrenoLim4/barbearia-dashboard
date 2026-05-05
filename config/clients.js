04e0a93c5d232347f5b7192bb4ebcc018ebb8738
EDUARDO BRENO MATOS DE LIMA <010861L3@sefaz-ce.gov.br>
2026-05-05 09:19:22 -0300

    Refactor: Renomear clientes, atualizar cores do tema e botoes de acao com icones

--- config/clients.js
+++ config/clients.js
@@ -1,6 +1,6 @@
 const CLIENTS = {
-  "6BJWKQ3I50ox2sBs": "isaque",
-  "ckdUkctZTFFS5U34": "gustavo"
+  "6BJWKQ3I50ox2sBs": "isaqui",
+  "ckdUkctZTFFS5U34": "gusttavo"
 };
 
 window.CLIENTS = CLIENTS;
---
+++ config/isaqui.js
@@ -0,0 +1,15 @@
+window.APP_CONFIG = {
+  cliente: "Barbearia Isaque Souza",
+  logo: "logo_barber_isaqui.jpeg",
+  apiUrl: "https://script.google.com/macros/s/AKfycbyb4Wk-R3_svRKbZ_mEeFpIl276bOc9Di6ai_pUBmVDcyh2YrxFQ0Ub1bBAwMZe-DbCrQ/exec",
+  theme: {
+    primary: "#0D3B66",
+    secondary: "#F2A541",
+    background: "#0A2E4D",
+    surface: "#123E63",
+    text: "#FFFFFF",
+    textMuted: "#C7D3E0",
+    accent: "#E63946",
+    highlight: "#305C85"
+  }
+};
